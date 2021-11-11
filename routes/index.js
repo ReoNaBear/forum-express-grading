f3621d2478e8d92dd10f9a4b31a22b9794e9c255
ReoNaBear <adamsimprove1108@gmail.com>
2021-11-11 20:29:16 +0800

    install method_override and Add admin restaurant put feature

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,8 @@ module.exports = (app, passport) => {
 
   app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
 
+  app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
+
   app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
 
   app.get('/signup', userController.signUpPage)
