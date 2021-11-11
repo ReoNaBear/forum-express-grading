142e4d2597f3ecc2483346a346ca3d24bd8c957d
ReoNaBear <adamsimprove1108@gmail.com>
2021-11-11 18:05:58 +0800

    Add admin restaurant create form

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,7 @@ module.exports = (app, passport) => {
 
   // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
@@ -35,4 +36,6 @@ module.exports = (app, passport) => {
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
   app.get('/logout', userController.logout)
+
+
 }
