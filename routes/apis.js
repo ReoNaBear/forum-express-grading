e86944c879d5b30790bd9a54107165345a8a731b
ReoNaBear <adamsimprove1108@gmail.com>
2021-11-26 22:14:17 +0800

    DELETE /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -7,6 +7,9 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 
+
+router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+
 router.get('/admin/categories', categoryController.getCategories)
 
 module.exports = router
