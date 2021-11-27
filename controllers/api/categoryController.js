0d19e8f4cfb62071ace12b80b03b27a1d89fcdb1
ReoNaBear <adamsimprove1108@gmail.com>
2021-11-27 17:10:35 +0800

    PUT /api/admin/categories/:id

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -6,10 +6,16 @@ let categoryController = {
     categoryService.getCategories(req, res, (data) => {
       return res.json(data)
     })
-  }, postCategory: (req, res) => {
+  },
+  postCategory: (req, res) => {
     categoryService.postCategory(req, res, (data) => {
       return res.json(data)
     })
-  }
+  },
+  putCategory: (req, res) => {
+    categoryService.putCategory(req, res, (data) => {
+      return res.json(data)
+    })
+  },
 }
 module.exports = categoryController
