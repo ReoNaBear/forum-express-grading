6d315222af5347810ce316c71c2cc88b2e055239
ReoNaBear <adamsimprove1108@gmail.com>
2021-11-27 16:50:19 +0800

    PUT /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -12,6 +12,7 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
